7451476dd54805adeffa9518113291c4be36857d
Praveen <praveen.arunachalam74@gmail.com>
2021-11-30 23:33:48 +0530

    added more alias and func

--- utils/aliases.sh
+++ utils/aliases.sh
@@ -7,6 +7,11 @@ wsmount() {
 
 ### alias
 # navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+# $OLDPWD location of last working directory
+alias back='cd $OLDPWD'
 alias play='$(wsmount); cd "/media/praveen/workspace/playground"';
 alias ws='$(wsmount); cd "/media/praveen/workspace/side_projects"';
 
@@ -15,6 +20,9 @@ alias ports='netstat -tulanp'
 
 # system utils
 alias c='clear'
+alias ffind="find . -type f -name $@"
+alias dfind="find . -type d -name $@"
+alias reload="[[ -e ~/.bashrc ]] && source ~/.bashrc"
 alias trash='gio trash -f $@'
 
 echo "loaded aliases";
--- utils/func.sh
+++ utils/func.sh
@@ -2,10 +2,18 @@
 
 ### user defined functions
 
+## Programming
 # java package manager - switch java versions
 jpm() {
   sudo update-alternatives --config javac;
   sudo update-alternatives --config java;
+}
+
+## System utils
+# create directory and move to it
+mkcd() {
+	mkdir -p $1;
+	cd $1
 }
 
 echo "loaded functions";
